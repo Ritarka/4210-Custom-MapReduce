afce8b63e271a11f297dab0600ea12ddd14b3892
Patel, Aditi N <apatel768@gatech.edu>
2023-11-17 16:43:34 -0500

    initial mr_tasks

--- src/CMakeFiles/mapreducelib.dir/DependInfo.cmake
+++ src/CMakeFiles/mapreducelib.dir/DependInfo.cmake
@@ -8,12 +8,13 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/ritarka/cs4210/4210-proj4/src/mapreduce.cc" "src/CMakeFiles/mapreducelib.dir/mapreduce.cc.o" "gcc" "src/CMakeFiles/mapreducelib.dir/mapreduce.cc.o.d"
-  "/home/ritarka/cs4210/4210-proj4/src/mapreduce_impl.cc" "src/CMakeFiles/mapreducelib.dir/mapreduce_impl.cc.o" "gcc" "src/CMakeFiles/mapreducelib.dir/mapreduce_impl.cc.o.d"
+  "/home/apatel768/4210-proj4/src/mapreduce.cc" "src/CMakeFiles/mapreducelib.dir/mapreduce.cc.o" "gcc" "src/CMakeFiles/mapreducelib.dir/mapreduce.cc.o.d"
+  "/home/apatel768/4210-proj4/src/mapreduce_impl.cc" "src/CMakeFiles/mapreducelib.dir/mapreduce_impl.cc.o" "gcc" "src/CMakeFiles/mapreducelib.dir/mapreduce_impl.cc.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/apatel768/4210-proj4/src/CMakeFiles/p4protolib.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- src/mr_tasks.h
+++ src/mr_tasks.h
@@ -2,6 +2,10 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+//string stream for breaking the words
 
 /* CS6210_TASK Implement this data structureas per your implementation.
 		You will need this when your worker is running the map task*/
@@ -14,11 +18,16 @@ struct BaseMapperInternal {
 		void emit(const std::string& key, const std::string& val);
 
 		/* NOW you can add below, data members and member functions as per the need of your implementation*/
+		//for mapper, write the intermediate pairs to an intermediate file than can be passed to the reduce function
+		void writeInterToFile(const std::string& filePath);
+		//have our intermediate pairs
+		std::vector<std::pair<std::string, std::string>> intermediatePairs;
 };
 
 
 /* CS6210_TASK Implement this function */
 inline BaseMapperInternal::BaseMapperInternal() {
+//leave the constructor empty?
 
 }
 
@@ -26,7 +35,22 @@ inline BaseMapperInternal::BaseMapperInternal() {
 /* CS6210_TASK Implement this function */
 inline void BaseMapperInternal::emit(const std::string& key, const std::string& val) {
 	std::cout << "Dummy emit by BaseMapperInternal: " << key << ", " << val << std::endl;
+	//create a key, value pair and push it into the int pair vector
+	intermediatePairs.emplace_back(key, val);
 }
+inline void BaseMapperInternal::writeInterToFile(const std:: string& filePath){
+	std::ofstream intermediateFile(filePath);
+	if(intermediateFile.is_open()) {
+		for (const auto& pair : intermediatePairs) {
+			//write each pair in the file
+			intermediateFile << pair.first << " " << pair.second << "\n";
+		}
+		intermediateFile.close();
+	} else {
+		std::cerr <<"Error: unable to open the file for writing intermediate data\n";
+	}
+}
+	
 
 
 /*-----------------------------------------------------------------------------------------------*/
@@ -43,11 +67,16 @@ struct BaseReducerInternal {
 		void emit(const std::string& key, const std::string& val);
 
 		/* NOW you can add below, data members and member functions as per the need of your implementation*/
+		//output file to write the result of the reduce function
+		void writeOutputToFile(const std::string& filePath);
+		//vector of pairs for final output
+		std::vector<std::pair<std::string, std::string>> finalPairs;
 };
 
 
 /* CS6210_TASK Implement this function */
 inline BaseReducerInternal::BaseReducerInternal() {
+//leaving the constructor empty for now?
 
 }
 
@@ -55,4 +84,17 @@ inline BaseReducerInternal::BaseReducerInternal() {
 /* CS6210_TASK Implement this function */
 inline void BaseReducerInternal::emit(const std::string& key, const std::string& val) {
 	std::cout << "Dummy emit by BaseReducerInternal: " << key << ", " << val << std::endl;
+	finalPairs.emplace_back(key, val);
+}
+//implementation of writeOutputToFile()
+inline void BaseReducerInternal::writeOutputToFile(const std:: string& filePath){
+	std::ofstream outputFile(filePath);
+	if(outputFile.is_open()){
+		for(const auto& pair: finalPairs){
+			outputFile << pair.first << " " << pair.second << "\n";
+		}
+		outputFile.close();
+	} else {
+		std::cerr << "Error: Unable to open file for writing final output\n";
+	}
 }
